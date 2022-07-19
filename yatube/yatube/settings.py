a73d3bf9d7b38333f3347e1b9261c13f787b6c03
Wingorman <wingorman1307@gmail.com>
2022-07-19 14:32:43 +0800

    download media static

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "-!&%f8%gifg&u9u$i%d_tuzehmghsf5$=-0$fi=5-ncvj8eclb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
